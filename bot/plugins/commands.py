b8724d8ec6e52ed8633c94b3539a256d13006c8f
me is noob <79394737+Devansh20055@users.noreply.github.com>
2021-12-28 16:36:27 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/team_silent_king"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/team_silent_king'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/Devansh20055/filter-bot-for-tg-v2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/ITS_NOT_ROMEO')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
