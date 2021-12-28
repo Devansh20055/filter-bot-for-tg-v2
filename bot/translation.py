248a8381aaaf2cf1edf5fdec92518ff7e0a112f1
me is noob <79394737+Devansh20055@users.noreply.github.com>
2021-12-28 16:31:45 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -10,7 +10,7 @@ class Translation(object):
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@team_silent_king
 </i>"""    
     
     HELP_TEXT = """
@@ -18,7 +18,7 @@ For More Details Click Help Button Below..
 
 <i>
 -> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
+
 </i>
 
 <b>Bot Commands (Works Only In Groups) :</b>
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@team_silent_king
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/@team_silent_king">@ITS_NOT_ROMEO</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/Devansh20055/filter-bot-for-tg-v2">Click Me</a></i>
 """
